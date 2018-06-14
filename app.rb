af818e7c6de46d20f9692f508935119550b9a1e4
randihi <randihigashi@gmail.com>
2018-06-14 19:52:07 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 get '/' do
+<<<<<<< HEAD
   erb :index
+=======
+  erb: index
+>>>>>>> 8315befff7703e09f539815ecb10fd1e42389d41
 end
 
 end
